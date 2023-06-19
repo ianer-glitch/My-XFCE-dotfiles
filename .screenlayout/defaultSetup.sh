da3716e5538dc33d463ecfadb25c5e6dc4a0acc8
ianer <ianrepker@gmail.com>
2023-06-19 20:25:36 -0300

    initial dotfiles xfce

---
+++ .screenlayout/defaultSetup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output DP-0 --mode 1024x768 --pos 3520x0 --rotate left --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --mode 1024x768 --pos 4288x0 --rotate left --output DP-4 --primary --mode 1600x900 --pos 1920x0 --rotate normal --output DP-5 --off
